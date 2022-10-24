1da200bc82dcc3d290a0de36be05bda005812153
limjongseung <jongseungim5348@gmail.com>
2022-10-24 16:14:17 +0900

    js_day02

---
+++ 예제/변수/ex_4_10.js
@@ -0,0 +1,19 @@
+//!4_10
+console.log(score);
+score = 80; //값의 할당
+var score; //변수의 선언
+console.log(score); //??
+// [Running] node "/Users/imjongseung/Desktop/SBS_LJS/javascript_edu/예제/변수/tempCodeRunnerFile.js"
+// undefined
+// 80
+
+//!4_11
+var score = 80; //변수 선언과 값의 할당
+score = 90; //값의 재할당
+
+
+
+
+
+
+
