b2a2bb8fd15b8b7235f242fa4de336ef43047d60
lim5348 <jongseungim5348@gmail.com>
2022-10-31 16:06:40 +0900

    javascript problem

--- inf_javascript/lecture.problem_array.js
+++ inf_javascript/lecture.problem_array.js
@@ -1,193 +1,149 @@
-let animals= [
+let animals = [
+	"Aardvark",
+	"Albatross",
+	"Alligator",
+	"Alpaca",
+	"Ant",
+	"Ape",
+	"Armadillo",
+	"Donkey",
+	"Baboon",
+	"Badger",
+	"Barracuda",
+	"Bat",
+	"Bear",
+	"Beaver",
+	"Bee",
+	"Bison",
+	"Cat",
+	"Caterpillar",
+	"Cattle",
+	"Chamois",
+	"Cheetah",
+	"Chicken",
+	"Chimpanzee",
+	"Chinchilla",
+	"Chough",
+	"Clam",
+	"Cobra",
+	"Cockroach",
+	"Cod",
+	"Cormorant",
+	"Dugong",
+	"Dunlin",
+	"Eagle",
+	"Echidna",
+	"Eel",
+	"Eland",
+	"Elephant",
+	"Elk",
+	"Emu",
+	"Falcon",
+	"Ferret",
+	"Finch",
+	"Fish",
+	"Flamingo",
+	"Fly",
+	"Fox",
+	"Frog",
+	"Gaur",
+	"Gazelle",
+	"Gerbil",
+	"Giraffe",
+	"Grasshopper",
+	"Heron",
+	"Herring",
+	"Hippopotamus",
+	"Hornet",
+	"Horse",
+	"Kangaroo",
+	"Kingfisher",
+	"Koala",
+	"Kookabura",
+	"Moose",
+	"Narwhal",
+	"Newt",
+	"Nightingale",
+	"Octopus",
+	"Okapi",
+	"Opossum",
+	"Quail",
+	"Quelea",
+	"Quetzal",
+	"Rabbit",
+	"Raccoon",
+	"Rail",
+	"Ram",
+	"Rat",
+	"Raven",
+	"Red deer",
+	"Sandpiper",
+	"Sardine",
+	"Sparrow",
+	"Spider",
+	"Spoonbill",
+	"Squid",
+	"Squirrel",
+	"Starling",
+	"Stingray",
+	"Tiger",
+	"Toad",
+	"Whale",
+	"Wildcat",
+	"Wolf",
+	"Worm",
+	"Wren",
+	"Yak",
+	"Zebra",
+];
+//1 어레이 마지막 아이템 "Zebra"제거하기
+//pop() : 맨마지막 아이템을 뺌
+animals.pop();
+console.log(animals)
 
-    "Aardvark",
+//주어진 어레이에 "Dog"추가하기
+animals.push("Dog")
+console.log(animals)
 
-    "Albatross",
+//주어진 어레이에 "Mosquito","Mouse","Mule"	추가하기
+animals.push("Mosquito","Mouse","Mule")
+console.log(animals)
 
-    "Alligator",
+//해당 어레이에 Human이 있는가?
+console.log(animals.includes("Human"))
+// 정담 없음
 
-    "Alpaca",
+//해당 어레이에 Cat 이 있는가?
+console.log(animals.includes("Cat"))
+// 정답 있음
 
-    "Ant",
+//'"Red deer" -> "Deer"
+console.log(animals.indexOf("Red deer"))
+console.log(animals[77])
+animals[77] = "Deer"
+console.log(animals[77])
 
-    "Ape",
+//"Spider"부터 3개의 아이템을 기존 어레이에서 제거하시오
+console.log(animals.indexOf("Spider"))
+animals.splice(81,3)
+console.log(animals[81])
 
-    "Armadillo",
+//Tiger 이후의  값을 제거하시오
+// animals.splice(animals.indexOf("Tiger"))
+console.log(animals.indexOf("Tiger"))
+console.log(animals[84])
+console.log(animals.indexOf("Mule"))
+animals.splice(84,12)
+console.log(animals)
 
-    "Donkey",
+//Baboon 부터 Bison 까지 가져와 새로운 어레이에 저장하시오
+console.log(animals.indexOf("Baboon"))
+console.log(animals.indexOf("Bison"))
+let animalsB = animals.slice(8,16)
+console.log(animalsB)
 
-    "Baboon",
 
-    "Badger",
 
-    "Barracuda",
 
-    "Bat",
 
-    "Bear",
 
-    "Beaver",
-
-    "Bee",
-
-    "Bison",
-
-    "Cat",
-
-    "Caterpillar",
-
-    "Cattle",
-
-    "Chamois",
-
-    "Cheetah",
-
-    "Chicken",
-
-    "Chimpanzee",
-
-    "Chinchilla",
-
-    "Chough",
-
-    "Clam",
-
-    "Cobra",
-
-    "Cockroach",
-
-    "Cod",
-
-    "Cormorant",
-
-    "Dugong",
-
-    "Dunlin",
-
-    "Eagle",
-
-    "Echidna",
-
-    "Eel",
-
-    "Eland",
-
-    "Elephant",
-
-    "Elk",
-
-    "Emu",
-
-    "Falcon",
-
-    "Ferret",
-
-    "Finch",
-
-    "Fish",
-
-    "Flamingo",
-
-    "Fly",
-
-    "Fox",
-
-    "Frog",
-
-    "Gaur", 
-
-    "Gazelle",
-
-    "Gerbil",
-
-    "Giraffe",
-
-    "Grasshopper",
-
-    "Heron",
-
-    "Herring",
-
-    "Hippopotamus",
-
-    "Hornet",
-
-    "Horse",
-
-    "Kangaroo",
-
-    "Kingfisher",
-
-    "Koala",
-
-    "Kookabura",
-
-    "Moose",
-
-    "Narwhal",
-
-    "Newt",
-
-    "Nightingale",
-
-    "Octopus",
-
-    "Okapi",
-
-    "Opossum", 
-
-    "Quail",
-
-    "Quelea",
-
-    "Quetzal",
-
-    "Rabbit", 
-
-    "Raccoon",
-
-    "Rail",
-
-    "Ram",
-
-    "Rat",
-
-    "Raven",
-
-    "Red deer",
-
-    "Sandpiper",
-
-    "Sardine",
-
-    "Sparrow",
-
-    "Spider",
-
-    "Spoonbill",
-
-    "Squid",
-
-    "Squirrel",
-
-    "Starling",
-
-    "Stingray",
-
-    "Tiger",
-
-    "Toad",
-
-    "Whale",
-
-    "Wildcat",
-
-    "Wolf",
-
-    "Worm",
-
-    "Wren",
-
-    "Yak",
-
-    "Zebra"] 
