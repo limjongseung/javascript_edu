e8a5dc190895002b9bf75ab769d06d2c931ea22a
lim5348 <jongseungim5348@gmail.com>
2022-11-24 22:50:13 +0900

    최빈값

---
+++ programmers/javascript(최빈값구하기).js
@@ -0,0 +1,26 @@
+function solution(array) {
+    let map = new Map();
+  
+    if (array.length === 1) {
+      return array[0];
+    }
+  
+    // Map 객체 초기화
+    for (let i = 0; i <= Math.max(...array); i++) {
+      map.set(i, 0);
+    }
+  
+    for (let i = 0; i < array.length; i++) {
+      map.set(array[i], map.get(array[i]) + 1);
+    }
+  
+    const arr = Array.from(map.values());
+    const max = Math.max(...arr);
+  
+    // 최대값이 하나만 있다면 max, 여러 개 중복된다면 -1 출력
+    if (arr.indexOf(max) !== arr.lastIndexOf(max)) {
+      return -1;
+    } else {
+      return arr.indexOf(max);
+    }
+  }
