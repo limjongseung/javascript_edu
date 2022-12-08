d938dd1b6ee06893443a85ed026c9f03d2e12722
lim5348 <jongseungim5348@gmail.com>
2022-12-08 22:55:33 +0900

    news api

---
+++ news/main.js
@@ -0,0 +1,15 @@
+
+let news = [];
+const getlatestNews =async ()=>{
+    let url = new URL("https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10");
+
+    let header = new Headers({"x-api-key" : "amG_c3TcHIIbTrzuK9yC9-sYhYoXSypke4t5OCni29s"})
+    let response = await fetch(url,{headers:header}) //ajax, ftch
+    let data = response.json();
+    console.log(data);
+
+    console.log(data.articles);
+
+};
+
+getlatestNews();
