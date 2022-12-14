57e453bd5f2a8300bb66c076aa83db8b2e8cbfb4
lim5348 <jongseungim5348@gmail.com>
2022-12-14 23:20:10 +0900

    javascript

--- news/main.js
+++ news/main.js
@@ -1,35 +1,55 @@
 let news = [];
+let menus = document.querySelectorAll(".menus button");
+menus.forEach(menu=> menu.addEventListener("click", (event)=>{
+    getNewsByTopic(event);
+}))
 const getlatestNews =async ()=>{
-    let url = new URL("https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business&page_size=10");
+    let url = new URL("https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=sport&page_size=10");
 
     let header = new Headers({
-        "x-api-key" : 'amG_c3TcHIIbTrzuK9yC9-sYhYoXSypke4t5OCni29s'
+        "x-api-key" : 'ATMkH6Oj5yYSEO4k64qS5s-Jiy_0BLSQFYCbDwbP7Vk'
     });
     let response = await  fetch(url, {headers: header}); //ajax, ftch
     let data = await response.json();
-    news = data.articles;
+    news = data.articles
     console.log(news);
-    console.log(data.media);
-
     render();
 };
 
+const getNewsByTopic = async (event)=>{
+    console.log("클릭됨", event.target.textContent);
+    let topic  = event.target.textContent.toLowerCase();
 
-const render  = () =>{
+    let header = new Headers({
+        "x-api-key" : 'ATMkH6Oj5yYSEO4k64qS5s-Jiy_0BLSQFYCbDwbP7Vk'
+    });
+    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&page_size=10&topic=${topic}`)
+    console.log(url);
+    let response = await  fetch(url, {headers: header});
+    let data = await response.json();
+    news = data.articles
+    render();
+}
+
+
+const render  = () => {
     let newsHTML = "";
-    newsHTML = news.map (news =>{
+    newsHTML = news.map (item =>{
         return   `<div class="row news">
         <div class="col-lg-4">
-            <img  class="news_img "src="${data.media}{" alt="">
+            <img  class="news_img "src="${item.media}{" alt="">
         </div>
         <div class="col-lg-8">
-            <h2>${data.title}</h2>
-            <p>배고프다...</p>
-            <div>졸려요</div>
+            <h2>${item.title}</h2>
+            <p>${item.summary}</p>
+            <div>
+            ${item.rights} ,  ${item.published_date}
+            </div>
         </div>
     </div>`           
     });
-}
+    document.getElementById("news_board").innerHTML = newsHTML;
+};
 
 
 
