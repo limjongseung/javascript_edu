c424c8bee8d3f489d1d2bb49b1ec03c3a9d86759
lim5348 <jongseungim5348@gmail.com>
2022-12-18 23:08:33 +0900

    javascript

--- news/main.js
+++ news/main.js
@@ -2,35 +2,34 @@ let news = [];
 let menus = document.querySelectorAll(".menus button");
 menus.forEach(menu=> menu.addEventListener("click", (event)=>{
     getNewsByTopic(event);
-}))
+}));
 let searchBtn = document.getElementById("search_button")
-
-const getlatestNews =async ()=>{
-    let url = new URL("https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=sport&page_size=10");
-
+let url ="";
+// 각 함수에서 필요하 url를 만든다
+//api 호출 함수를 부른다.
+const getNews = async()=>{
     let header = new Headers({
-        "x-api-key" : 'ATMkH6Oj5yYSEO4k64qS5s-Jiy_0BLSQFYCbDwbP7Vk'
+        "x-api-key" : "UkVGZfSI9CyRG1ceVd18H8HW8BwsyHIt6z5QKYgeuco"
     });
     let response = await  fetch(url, {headers: header}); //ajax, ftch
     let data = await response.json();
+    console.log(data) ;
     news = data.articles
     console.log(news);
     render();
+}
+const getlatestNews =async ()=>{
+    url = new URL("https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=10");
+    getNews();
+  
 };
 
 const getNewsByTopic = async (event)=>{
     console.log("클릭됨", event.target.textContent);
     let topic  = event.target.textContent.toLowerCase();
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=10`);
+    getNews();
 
-    let header = new Headers({
-        "x-api-key" : 'ATMkH6Oj5yYSEO4k64qS5s-Jiy_0BLSQFYCbDwbP7Vk'
-    });
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&page_size=10&topic=${topic}`)
-    console.log(url);
-    let response = await  fetch(url, {headers: header});
-    let data = await response.json();
-    news = data.articles
-    render();
 }
 const getNewsBykeyword = async()=>{
     //1검색키워드 읽어오기
@@ -40,17 +39,13 @@ const getNewsBykeyword = async()=>{
     //5. 데이터 가져오기
     //6 데이터 보여주기
     let keyword = document.getElementById('search_input').value;
-    console.log(keyword);
-    let url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&from='2021/12/15'&countries=CA&page_size=1`);
-    let response = await  fetch(url, {headers: header});
-    let data = await response.json();
-    news = data.articles
-    render();
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`);
+   getNews();
 }
 
 const render  = () => {
     let newsHTML = "";
-    newsHTML = news.map (item =>{
+    newsHTML = news.map(item =>{
         return   `<div class="row news">
         <div class="col-lg-4">
             <img  class="news_img "src="${item.media}{" alt="">
