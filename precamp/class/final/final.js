0ef6fc828d6eaf85a2e4d2ab1b75c8d4ae81bb08
lim5348 <jongseungim5348@gmail.com>
2023-01-30 23:47:09 +0900

    cyworld

--- precamp/class/final/final.js
+++ precamp/class/final/final.js
@@ -1,8 +1,15 @@
-let Tel1 = document.getElementById("tel1").value;
-const FocusTel1 =()=>{
-    console.log("ㅇㅇ")
+
+const FocusTel1 = ()=>{
+    let Tel1 = document.getElementById("tel1").value;
     if(Tel1.length ===3){
         document.getElementById("tel2").focus();
+    }
+};
+
+const FocusTel2 = () =>{
+    let Tel2 = document.getElementById("tel2").value;
+    if(Tel2.length ===4){
+        document.getElementById("tel3").focus();
     }
 }
 
