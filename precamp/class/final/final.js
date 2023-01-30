74d4cf84d50f55bca36fe16993e6ddece59d003e
lim5348 <jongseungim5348@gmail.com>
2023-01-30 22:58:25 +0900

    final

---
+++ precamp/class/final/final.js
@@ -0,0 +1,11 @@
+let Tel1 = document.getElementById("tel1").value;
+const FocusTel1 =()=>{
+    console.log("ㅇㅇ")
+    if(Tel1.length ===3){
+        document.getElementById("tel2").focus();
+    }
+}
+
+
+
+
