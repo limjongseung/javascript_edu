e269d3493015f771e2c9d37944aec32aef7a98d1
lim5348 <jongseungim5348@gmail.com>
2023-01-31 23:27:02 +0900

    cellphone certificate

--- precamp/class/final/final.js
+++ precamp/class/final/final.js
@@ -12,7 +12,19 @@ const FocusTel2 = () =>{
         document.getElementById("tel3").focus();
     }
 }
+const BtnAble = () =>{
+    let Tel3 = document.getElementById("tel3").value;
+    if(Tel3.length ===4){
+        document.getElementById("btn_submit").disabled = false;
+        document.getElementById("btn_complete").disabled = false;
+    }
+}
+const submitNum = () =>{
+    document.getElementById("token").innerHTML =String( Math.floor(Math.random()*1000000)).padStart(6,"0");
+}
 
+ // 휴대전화를 인증번호 전송 버튼이 활성화 버튼을 누르면 토큰을 생성하여 화면에 보여주세요
+ //
 
 
 
