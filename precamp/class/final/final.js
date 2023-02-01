bc13ca9480607abd4bccf78a6a65e81e7bcb1131
limjongseung <jongseungim5348@gmail.com>
2023-02-01 22:59:14 +0900

    javascript

--- precamp/class/final/final.js
+++ precamp/class/final/final.js
@@ -21,10 +21,24 @@ const BtnAble = () =>{
 }
 const submitNum = () =>{
     document.getElementById("token").innerHTML =String( Math.floor(Math.random()*1000000)).padStart(6,"0");
+    let time = 10;
+    let timer 
+    timer = setInterval(function(){
+      let min = Math.floor(time/60);
+      let second = String(Math.floor(time%60)).padStart(2,"0")
+      time  = time -1;
+      if(time> 0){
+          document.getElementById("timer").innerText = min +":"+second;
+      } else{
+        clearInterval(timer);
+        time =180;
+      }
+    },1000)
 }
 
  // 휴대전화를 인증번호 전송 버튼이 활성화 버튼을 누르면 토큰을 생성하여 화면에 보여주세요
- //
+ //3분 타이머도 동작해야 합니다.
+
 
 
 
