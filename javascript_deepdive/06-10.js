a4cf440ced0a2b629cc08a7b68594bf957b1ded3
limjongseung <jongseungim5348@gmail.com>
2023-04-26 16:43:30 +0900

    자바스크립트공부

---
+++ javascript_deepdive/06-10.js
@@ -0,0 +1,2 @@
+var template = '<ul>\n\t<li><a href="#">home</a></li>\n</ul>'
+console.log(template);
---
+++ javascript_deepdive/07-09.js
@@ -0,0 +1,18 @@
+var x;
+ x= 10
+ console.log(x);
+ x +=5 ;
+ console.log(x);
+ x-=5;
+ console.log(x);
+ x*=5;
+ console.log(x);
+ x /=5;
+ console.log(x);
+ x %=5;
+ console.log(x);
+ var str = 'My name is ';
+
+ //문자열 연결 연산자
+ str += "Lee";
+ console.log(str);
